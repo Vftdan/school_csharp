d062d2bb8dc0a67d08ad594a123349e3c2366705
Vftdan <Vftdan.feed@gmail.com>
2016-09-21 15:11:44 +0200

    Add files via upload

---
+++ 20160921/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+class class1
+{
+    public static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y;
+        if (x <= -0.5)  y = 0.5;  else
+            if (x <= 0)  y = x+1;  else
+                if (x <= 1)  y = x * x - 1; 
+                else
+                 y = x - 1; 
+        
+        Console.WriteLine(y);           
+        Console.ReadKey(true);               
+    }
+}
+
---
+++ 20160921/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double R = 2.0;
+            double x = Double.Parse(Console.ReadLine());
+            double y = Double.Parse(Console.ReadLine());
+            double d=Math.Sqrt(x*x+y*y);
+            bool ret = true;
+            if (d <= R)
+            {
+
+            }
+            else { ret = false; }
+            Console.WriteLine(ret);
+            Console.ReadKey(true); 
+        }
+    }
+}
